15bcb6341ca81179fa63bf7c9a87216b16063445
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-06-09 22:36:57 +0630

    feat: store the review summary

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,11 +1,29 @@
 import type { Review } from '../generated/prisma/client';
 import { prisma } from '../routes';
+import dayjs from 'dayjs';
 
 export const reviewRepository = {
    async getReviews(productId: number, limit?: number): Promise<Review[]> {
       return await prisma.review.findMany({
          where: { productId },
          orderBy: { createdAt: 'desc' },
+      });
+   },
+
+   storeReviewSummary(productId: number, summary: string) {
+      const now = new Date();
+      const expiresAt = dayjs().add(7, 'days').toDate();
+      const data = {
+         content: summary,
+         expiresAt,
+         generatedAt: now,
+         productId,
+      };
+
+      return prisma.summary.upsert({
+         where: { productId },
+         create: data,
+         update: data,
       });
    },
 };
